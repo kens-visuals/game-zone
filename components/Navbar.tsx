7edb97120b29ed984f07f9b208e2d92c75f5ddcf
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-01-11 22:24:34 -0800

    add new link to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,6 +49,25 @@ export default function Navbar() {
               />
             </svg>
           </Link>
+
+          <Link href="/genres">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className={`h-6 w-6 text-primary-light ${
+                pathname === '/genres' && 'stroke-slate-50'
+              }`}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M3.75 12h16.5m-16.5 3.75h16.5M3.75 19.5h16.5M5.625 4.5h12.75a1.875 1.875 0 010 3.75H5.625a1.875 1.875 0 010-3.75z"
+              />
+            </svg>
+          </Link>
         </div>
 
         <button
