cf73060d0f2040de8a9532654b9393bb4a066529
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-01-16 18:13:53 -0800

    add condition for sign in

--- pages/bookmarks/index.tsx
+++ pages/bookmarks/index.tsx
@@ -1,12 +1,18 @@
-// Hooks
-import useUser from '../../hooks/useUser';
-import useUserBookmarks from '../../hooks/useUserBookmarks';
+import Link from 'next/link';
+
+// Bookmarks
 import GamesListContainer from '../../components/GamesListContainer';
 import GameCard from '../../components/GameCard';
+
+// Hooks
+import useUser from '../../hooks/useUser';
+import useAuth from '../../hooks/useAuth';
+import useUserBookmarks from '../../hooks/useUserBookmarks';
 
 export default function Bookmarks() {
   const user = useUser();
   const { status, data: bookmarks } = useUserBookmarks();
+  const { handleUserSignIn } = useAuth();
 
   //   const [query, setQuery] = useState('');
   //   const [results, setResults] = useState<any[]>([]);
@@ -35,16 +41,46 @@ export default function Bookmarks() {
 
   return (
     <div className="p-4">
-      <GamesListContainer>
-        {/* <input type="text" value={query} onChange={handleChange} /> */}
-        {user && bookmarks
-          ? bookmarks?.map((bookmark) => (
+      {!user.data ? (
+        <div className="flex flex-col items-center justify-center gap-4">
+          <h1>No bookmarks yet!</h1>
+
+          <button
+            type="button"
+            onClick={handleUserSignIn}
+            className="mr-2 mb-2 inline-flex items-center rounded-lg bg-[#4285F4] px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-[#4285F4]/90 focus:outline-none focus:ring-4 focus:ring-[#4285F4]/50"
+          >
+            <svg
+              className="mr-2 -ml-1 h-4 w-4"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="google"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 488 512"
+            >
+              <path
+                fill="currentColor"
+                d="M488 261.8C488 403.3 391.1 504 248 504 110.8 504 0 393.2 0 256S110.8 8 248 8c66.8 0 123 24.5 166.3 64.9l-67.5 64.9C258.5 52.6 94.3 116.6 94.3 256c0 86.5 69.1 156.6 153.7 156.6 98.2 0 135-70.4 140.8-106.9H248v-85.3h236.1c2.3 12.7 3.9 24.9 3.9 41.4z"
+              />
+            </svg>
+            Sign in with Google
+          </button>
+        </div>
+      ) : (
+        <GamesListContainer>
+          {/* <input type="text" value={query} onChange={handleChange} /> */}
+          {!bookmarks.length ? (
+            <Link href="/">Go to games</Link>
+          ) : (
+            bookmarks?.map((bookmark) => (
               <div key={bookmark.createdAt}>
                 <GameCard details={bookmark} isFromBookmark />
               </div>
             ))
-          : null}
-        {/* {!results
+          )}
+          {/* {!results
               ? results.map((d) => (
                   <li key={createdAt}>
                     <Link href={`/game/${slug}`}>{name}</Link>{' '}
@@ -62,7 +98,8 @@ export default function Bookmarks() {
                     </button>
                   </li>
                 ))} */}
-      </GamesListContainer>
+        </GamesListContainer>
+      )}
     </div>
   );
 }
