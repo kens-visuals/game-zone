05550c4cbf5c1a61780fcafa7350fb4aa61b4900
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-01-19 22:18:03 -0800

    fix adding collection form

--- pages/add-new-collection/index.tsx
+++ pages/add-new-collection/index.tsx
@@ -1,4 +1,4 @@
-import { FormEvent, useState } from 'react';
+import { FormEvent, useId, useState } from 'react';
 
 // Components
 import ErrorMsg from '../../components/ErrorMsg';
@@ -8,6 +8,7 @@ import LoadingMsg from '../../components/LoadingMsg';
 import useCollections from '../../hooks/useCollections';
 
 export default function AddNewCollection() {
+  const isPublicId = useId();
   const { addNewCollection, removeCollection, collections, status } =
     useCollections();
 
@@ -21,6 +22,8 @@ export default function AddNewCollection() {
     e.preventDefault();
 
     if (collectionInfo.name) addNewCollection({ ...collectionInfo });
+
+    setCollectionInfo({ name: '', description: '', isPublic: true });
   };
 
   if (status === 'loading') return <LoadingMsg size={5} />;
@@ -29,7 +32,7 @@ export default function AddNewCollection() {
   return (
     <div className="p-4">
       <h1 className="mb-4 text-h1">Collections</h1>
-      <form action="#" onSubmit={handleSubmit}>
+      <form action="#" onSubmit={handleSubmit} className="flex flex-col gap-2 ">
         <input
           required
           type="text"
@@ -41,10 +44,10 @@ export default function AddNewCollection() {
             }))
           }
           placeholder="Collection name, i.g. Fav Games"
-          className="w-full border-b bg-transparent py-1 text-white placeholder:opacity-50 focus:border-b-primary-light focus-visible:outline-none"
+          className="w-full border-b bg-transparent bg-primary-dark py-1 text-white placeholder:opacity-50 focus:border-b-primary-light focus-visible:outline-none"
         />
         <textarea
-          cols={30}
+          cols={10}
           rows={10}
           value={collectionInfo.description}
           onChange={(e) =>
@@ -55,21 +58,39 @@ export default function AddNewCollection() {
           }
           name="collections_description"
           placeholder="This collection is..."
-          className="mt-4 w-full border-b bg-transparent py-1 text-white placeholder:opacity-50 focus:border-b-primary-light focus-visible:outline-none"
+          className="w-full border-b bg-transparent py-1 text-white placeholder:opacity-50 focus:border-b-primary-light focus-visible:outline-none"
         />
         {/* {!collectionName && <span>Please name your collection!</span>} */}
-        <input
-          type="checkbox"
-          name="isPublic"
-          checked={collectionInfo.isPublic}
-          onChange={() =>
-            setCollectionInfo((prevState) => ({
-              ...prevState,
-              isPublic: !prevState.isPublic,
-            }))
-          }
-        />
-        <button type="button" onClick={handleSubmit}>
+
+        <label
+          htmlFor={`${isPublicId}-public-id`}
+          className="relative mt-2 inline-flex cursor-pointer items-center"
+        >
+          <input
+            id={`${isPublicId}-public-id`}
+            type="checkbox"
+            name="isPublic"
+            className="peer sr-only"
+            checked={collectionInfo.isPublic}
+            onChange={() =>
+              setCollectionInfo((prevState) => ({
+                ...prevState,
+                isPublic: !prevState.isPublic,
+              }))
+            }
+          />
+          <div className="peer h-6 w-11 rounded-full border-gray-600 bg-gray-700 after:absolute after:top-0.5 after:left-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-gray-300 after:bg-white after:transition-all after:content-[''] peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white peer-focus:ring-4 peer-focus:ring-blue-800" />
+
+          <span className="ml-3 text-body-2 font-medium text-white">
+            Make it public?
+          </span>
+        </label>
+
+        <button
+          type="button"
+          onClick={handleSubmit}
+          className="mt-4 w-full rounded-md bg-primary-dark p-4 text-h3 text-white"
+        >
           Create new collection
         </button>
       </form>
