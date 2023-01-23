bc7df0bbd0a434320c53e13d17c7eee859b8ef39
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-01-22 16:46:04 -0800

    add ordering functionality

--- components/GamesList.tsx
+++ components/GamesList.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { GetStaticProps } from 'next';
 import { dehydrate, QueryClient, useInfiniteQuery } from 'react-query';
 
@@ -17,16 +18,34 @@ interface Games {
   results: GameInterface[];
 }
 
-const fetchGames = async ({ pageParam = 1 }): Promise<GameInterface[]> => {
+const fetchGames = async ({
+  option = 'relevance',
+  pageParam = 1,
+}): Promise<GameInterface[]> => {
   const apiKey = process.env.NEXT_PUBLIC_RAWG_API_KEY;
+
   const { data } = await RAWG.get<Games>(
-    `/games/lists/main?key=${apiKey}&discover=true&ordering=-relevance&page_size=40&page=${pageParam}`
+    `/games/lists/main?&discover=true&ordering=-${option}&page_size=40&page=${pageParam}&key=${apiKey}`
   );
 
   return data?.results;
 };
 
 export default function GamesList() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [option, setOption] = useState('name');
+
+  const options = [
+    'relevance',
+    'name',
+    'released',
+    'added',
+    'created',
+    'updated',
+    'rating',
+    'metacritic',
+  ];
+
   const {
     data: games,
     isError,
@@ -34,13 +53,19 @@ export default function GamesList() {
     hasNextPage,
     fetchNextPage,
     isFetchingNextPage,
-  } = useInfiniteQuery(['getGames'], fetchGames, {
-    getNextPageParam: (_, allPages) => {
-      if (allPages.length < 10) return allPages.length + 1;
+  } = useInfiniteQuery(
+    ['getGames', option],
+    ({ pageParam = 1 }) => fetchGames({ option, pageParam }),
+    {
+      getNextPageParam: (_, allPages) => {
+        if (allPages.length < 10) return allPages.length + 1;
 
-      return undefined;
-    },
-  });
+        return undefined;
+      },
+    }
+  );
+
+  useEffect(() => setIsDropdownOpen(false), [isLoading]);
 
   if (isLoading)
     return (
@@ -53,12 +78,57 @@ export default function GamesList() {
 
   return (
     <div className="flex flex-col items-center gap-4 p-4 pb-14">
-      <h1 className="my-2 self-start text-h1">All Games</h1>
+      <div className="relative w-full">
+        <button
+          type="button"
+          onClick={() => setIsDropdownOpen((prevState) => !prevState)}
+          className="inline-flex w-full items-center justify-between rounded-lg bg-secondary px-4 py-2.5 text-center text-sm font-medium capitalize text-white hover:bg-secondary/80 focus:outline-none focus:ring-4 focus:ring-secondary/20 md:w-52"
+        >
+          Order by: {option}{' '}
+          <svg
+            className={`ml-2 h-4 w-4 transition-all duration-300 ${
+              isDropdownOpen && 'rotate-180'
+            }`}
+            aria-hidden="true"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M19 9l-7 7-7-7"
+            />
+          </svg>
+        </button>
+
+        {isDropdownOpen && (
+          <div className="absolute top-12 z-10 w-full divide-y divide-gray-100 overflow-hidden rounded-lg bg-white shadow dark:bg-gray-700 md:w-52">
+            <ul className="w-full divide-y divide-primary-dark text-sm text-gray-700 dark:text-gray-200">
+              {options.map((opt) => (
+                <li key={opt} className="w-full">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      setOption(opt);
+                    }}
+                    className="w-full p-4 capitalize hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >
+                    {opt}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
 
       <GamesListContainer>
         {games?.pages?.map((page) =>
           page.map((details) => (
-            <div key={details.slug}>
+            <div key={details.id}>
               <GameCard details={details} />
             </div>
           ))
