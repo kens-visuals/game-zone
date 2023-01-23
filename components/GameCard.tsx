10ef3566f3dd4308c42af0bbeb4689d1d947954d
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-01-22 19:52:54 -0800

    add placeholder img, change collections icon, and more minor stuff

--- components/GameCard.tsx
+++ components/GameCard.tsx
@@ -9,6 +9,9 @@ import useUserBookmarks from '../hooks/useUserBookmarks';
 import useBookmarkMutation from '../hooks/useBookmarkMutation';
 import useCollections from '../hooks/useCollections';
 
+// Assets
+import placeholderImage from '../public/assets/placeholder.avif';
+
 // Types
 import { GameInterface } from '../lib/types/game';
 
@@ -54,28 +57,13 @@ export default function GameCard({
       }`}
     >
       <div className="relative">
-        {!backgroundImage ? (
-          <div className="flex flex-col items-center justify-center p-10">
-            <span className="mb-2 inline-block text-2xl">NO IMAGE</span>
-            <svg
-              className="h-full w-full text-gray-200"
-              xmlns="http://www.w3.org/2000/svg"
-              aria-hidden="true"
-              fill="currentColor"
-              viewBox="0 0 640 512"
-            >
-              <path d="M480 80C480 35.82 515.8 0 560 0C604.2 0 640 35.82 640 80C640 124.2 604.2 160 560 160C515.8 160 480 124.2 480 80zM0 456.1C0 445.6 2.964 435.3 8.551 426.4L225.3 81.01C231.9 70.42 243.5 64 256 64C268.5 64 280.1 70.42 286.8 81.01L412.7 281.7L460.9 202.7C464.1 196.1 472.2 192 480 192C487.8 192 495 196.1 499.1 202.7L631.1 419.1C636.9 428.6 640 439.7 640 450.9C640 484.6 612.6 512 578.9 512H55.91C25.03 512 .0006 486.1 .0006 456.1L0 456.1z" />
-            </svg>
-          </div>
-        ) : (
-          <Image
-            src={backgroundImage}
-            alt={name}
-            width={200}
-            height={200}
-            className={`w-full ${isTrending && 'h-32 object-cover object-top'}`}
-          />
-        )}
+        <Image
+          src={backgroundImage || placeholderImage}
+          alt={name}
+          width={200}
+          height={200}
+          className={`w-full ${isTrending && 'h-32 object-cover object-top'}`}
+        />
 
         <button
           type="button"
@@ -166,21 +154,6 @@ export default function GameCard({
                 d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
               />
             </svg>
-            {/* Minus icon */}
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg> */}
           </>
         ) : (
           <>
@@ -199,21 +172,6 @@ export default function GameCard({
                 d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
               />
             </svg>
-            {/* Plus icon */}
-            {/* <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-6 w-6"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg> */}
           </>
         )}
       </button>
@@ -221,7 +179,7 @@ export default function GameCard({
       <button
         type="button"
         onClick={() => setIsDropdownOpen((prevState) => !prevState)}
-        className="inline-flex w-full items-center justify-center bg-blue-700 px-4 py-2.5 text-center text-sm font-medium text-white transition-all duration-300 hover:bg-blue-800"
+        className="inline-flex w-full items-center justify-center bg-secondary px-4 py-2.5 text-center text-sm font-medium text-white transition-all duration-300 hover:bg-secondary/80"
       >
         Collections{' '}
         <svg
@@ -244,18 +202,16 @@ export default function GameCard({
       </button>
 
       <div
-        className={`w-full rounded-b bg-primary-light ${
+        className={`w-full rounded-b bg-primary-dark ${
           isDropdownOpen ? 'inline-block' : 'hidden'
         }`}
       >
-        <ul className="h-fit overflow-y-auto p-4 text-white">
+        <ul className="h-fit space-y-2 overflow-y-auto p-4 text-white">
           {collections?.map((collection) => (
-            <li key={collection.id}>
-              {/* <img
-              className="mr-2 h-6 w-6 rounded-full"
-              src="/docs/images/people/profile-picture-1.jpg"
-              alt="Jese image"
-            /> */}
+            <li
+              key={collection.id}
+              className="flex items-center gap-2 rounded-lg bg-primary-light p-2 px-4 transition-all duration-300 hover:bg-primary-light/50"
+            >
               <button
                 type="button"
                 onClick={() =>
@@ -268,16 +224,15 @@ export default function GameCard({
                     genres,
                   })
                 }
-                className="flex w-full items-center justify-between"
+                className="flex w-full items-center gap-2"
               >
-                {collection.name}
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
                   viewBox="0 0 24 24"
                   strokeWidth={1.5}
                   stroke="currentColor"
-                  className="h-3 w-3"
+                  className="h-4 w-4"
                 >
                   <path
                     strokeLinecap="round"
@@ -285,27 +240,39 @@ export default function GameCard({
                     d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
                   />
                 </svg>
+                {collection.name}
+
+                {!collection.isPublic && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    className="ml-auto h-4 w-4 fill-primary-dark"
+                  >
+                    <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                    <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                    <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                  </svg>
+                )}
               </button>
             </li>
           ))}
         </ul>
         <Link
-          href="/add-new-collection"
+          href="/collections"
           className="flex w-full items-center justify-center gap-2 bg-black/20 p-2 text-body-2 backdrop-blur-lg backdrop-filter transition-all duration-300 hover:backdrop-blur-sm md:text-body-1"
         >
-          Create Collection
+          Create New Collection
           <svg
             xmlns="http://www.w3.org/2000/svg"
-            fill="none"
             viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
+            fill="currentColor"
             className="h-4 w-4 text-white"
           >
             <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+              fillRule="evenodd"
+              d="M15.75 2.25H21a.75.75 0 01.75.75v5.25a.75.75 0 01-1.5 0V4.81L8.03 17.03a.75.75 0 01-1.06-1.06L19.19 3.75h-3.44a.75.75 0 010-1.5zm-10.5 4.5a1.5 1.5 0 00-1.5 1.5v10.5a1.5 1.5 0 001.5 1.5h10.5a1.5 1.5 0 001.5-1.5V10.5a.75.75 0 011.5 0v8.25a3 3 0 01-3 3H5.25a3 3 0 01-3-3V8.25a3 3 0 013-3h8.25a.75.75 0 010 1.5H5.25z"
+              clipRule="evenodd"
             />
           </svg>
         </Link>
