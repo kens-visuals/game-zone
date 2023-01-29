dd90f5ac1ceec9c05df2914b270177b8ea25b281
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-01-28 23:19:47 -0800

    add developers pages

---
+++ pages/developer/[slug].tsx
@@ -0,0 +1,138 @@
+import { useRouter } from 'next/router';
+import { GetStaticPaths, GetStaticProps } from 'next';
+import {
+  dehydrate,
+  QueryClient,
+  useInfiniteQuery,
+  useQuery,
+} from 'react-query';
+
+// Components
+import Banner from '../../components/Banner';
+import ErrorMsg from '../../components/ErrorMsg';
+import GameCard from '../../components/GameCard';
+import LoadingMsg from '../../components/LoadingMsg';
+import GamesListContainer from '../../components/GamesListContainer';
+
+// Helpers
+import RAWG from '../../lib/rawg';
+
+// Types
+import { GameInterface, DataType } from '../../lib/types/game';
+
+const API_KEY = process.env.NEXT_PUBLIC_RAWG_API_KEY;
+
+const fetchDeveloper = async (slug: string): Promise<DataType> => {
+  const { data } = await RAWG.get<DataType>(
+    `developers/${slug}?key=${API_KEY}`
+  );
+
+  return data;
+};
+
+const fetchGames = async ({
+  developerSlug,
+  pageParam = 1,
+}: {
+  developerSlug: string;
+  pageParam: number;
+}): Promise<GameInterface[]> => {
+  const apiKey = process.env.NEXT_PUBLIC_RAWG_API_KEY;
+  const { data } = await RAWG.get<{ results: GameInterface[] }>(
+    `/games?key=${apiKey}&ordering=popularity&page_size=40&page=${pageParam}&developers=${developerSlug}`
+  );
+
+  return data?.results;
+};
+
+export default function Developer() {
+  const router = useRouter();
+  const developerSlug =
+    typeof router.query?.slug === 'string' ? router.query.slug : '';
+
+  const {
+    data: developer,
+    isError: isTagError,
+    isLoading: isTagLoading,
+  } = useQuery(
+    ['getDeveloper', developerSlug],
+    () => fetchDeveloper(developerSlug),
+    { enabled: !!developerSlug }
+  );
+
+  const {
+    data: games,
+    isError: isGamesError,
+    isLoading: isGamesLoading,
+
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(
+    ['getGames', developerSlug],
+    ({ pageParam = 1 }) => fetchGames({ developerSlug, pageParam }),
+    {
+      getNextPageParam: (_, allPages) => {
+        if (allPages.length < 10) return allPages.length + 1;
+
+        return undefined;
+      },
+    }
+  );
+
+  if (isTagLoading || isGamesLoading) return <LoadingMsg size={30} />;
+
+  if (isTagError || isGamesError) return <ErrorMsg />;
+
+  return (
+    <div>
+      {developer && <Banner data={developer} />}
+
+      <div className="p-4">
+        <GamesListContainer>
+          {games?.pages?.map((page) =>
+            page.map((details) => (
+              <div key={details.slug}>
+                <GameCard details={details} />
+              </div>
+            ))
+          )}
+        </GamesListContainer>
+
+        <button
+          type="button"
+          disabled={!hasNextPage || isFetchingNextPage}
+          onClick={() => hasNextPage && fetchNextPage()}
+          className="rounded-md bg-primary-light px-6 py-2 text-white"
+        >
+          {/* eslint-disable-next-line no-nested-ternary */}
+          {isFetchingNextPage
+            ? 'Loading more...'
+            : hasNextPage
+            ? 'Load More'
+            : 'Nothing more to load'}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const slug = context.params?.slug as string;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(['getDeveloper', slug], () =>
+    fetchDeveloper(slug)
+  );
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: [],
+  fallback: 'blocking',
+});
---
+++ pages/developers/index.tsx
@@ -0,0 +1,85 @@
+import { GetStaticProps } from 'next';
+import { dehydrate, QueryClient, useInfiniteQuery } from 'react-query';
+
+// Componentns
+import PageList from '../../components/PageList';
+import PageItem from '../../components/PageItem';
+import ErrorMsg from '../../components/ErrorMsg';
+import LoadingMsg from '../../components/LoadingMsg';
+
+// Helpers
+import RAWG from '../../lib/rawg';
+
+// Types
+import { DataType } from '../../lib/types/game';
+
+interface DataProps {
+  results: DataType[];
+}
+
+const fetchDevelopers = async ({ pageParam = 1 }): Promise<DataType[]> => {
+  const apiKey = process.env.NEXT_PUBLIC_RAWG_API_KEY;
+
+  const { data } = await RAWG.get<DataProps>(
+    `/developers?page_size=40&page=${pageParam}&key=${apiKey}`
+  );
+
+  return data?.results;
+};
+export default function Developers() {
+  const {
+    data: developers,
+    isError,
+    isLoading,
+    hasNextPage,
+    fetchNextPage,
+    isFetchingNextPage,
+  } = useInfiniteQuery(['getDevelopers'], fetchDevelopers, {
+    getNextPageParam: (_, allPages) => {
+      if (allPages.length < 10) return allPages.length + 1;
+
+      return undefined;
+    },
+  });
+
+  if (isLoading) return <LoadingMsg size={20} />;
+
+  if (isError) return <ErrorMsg />;
+
+  return (
+    <div className="flex flex-col items-center gap-4 pb-14">
+      <PageList>
+        {developers?.pages?.map((page) =>
+          page.map((data) => (
+            <PageItem key={data.name} route="developer" data={data} />
+          ))
+        )}
+      </PageList>
+
+      <button
+        type="button"
+        disabled={!hasNextPage || isFetchingNextPage}
+        onClick={() => hasNextPage && fetchNextPage()}
+        className="rounded-md bg-primary-light px-6 py-2 text-white"
+      >
+        {/* eslint-disable-next-line no-nested-ternary */}
+        {isFetchingNextPage
+          ? 'Loading more...'
+          : hasNextPage
+          ? 'Load More'
+          : 'Nothing more to load'}
+      </button>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery(['getDevelopers'], fetchDevelopers);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+  };
+};
