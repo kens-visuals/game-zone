5e86738528a81fbb7029b04399ca859aa29d5cf9
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-02-06 14:16:54 -0800

    remove useOutsideClick from Drawer

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -6,25 +6,13 @@ import UserProfile from './UserProfile';
 
 // Hooks
 import useUser from '../hooks/useUser';
-import useOutsideClick from '../hooks/useClickOutside';
 
-// Interfaces
-interface Props {
-  setIsDrawerOpen: (state: boolean) => void;
-}
-
-export default function Drawer({ setIsDrawerOpen }: Props) {
+export default function Drawer() {
   const { currentUser, isUserLoading } = useUser();
-
-  const callback = () => setIsDrawerOpen(false);
-  const ref = useOutsideClick(callback);
 
   return (
     <div className="fixed z-50 flex h-[96%] w-[calc(100vw_-_2rem)] flex-col items-end">
-      <aside
-        ref={ref}
-        className="mt-20 flex h-full w-60 min-w-max flex-col items-start gap-2 rounded-lg bg-primary-light/40 p-4 shadow-xl shadow-primary-dark backdrop-blur-lg backdrop-filter"
-      >
+      <aside className="mt-20 flex h-full w-60 min-w-max flex-col items-start gap-2 rounded-lg bg-primary-light/40 p-4 shadow-xl shadow-primary-dark backdrop-blur-lg backdrop-filter">
         {currentUser && <UserProfile />}
 
         <PagesNav />
