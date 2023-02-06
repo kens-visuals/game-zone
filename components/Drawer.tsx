77f7cadb26c9cfd296330c0fca79e76cec085745
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-02-06 13:17:19 -0800

    fix Drawer height on mobile

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -11,7 +11,7 @@ export default function Drawer() {
   const { currentUser, isUserLoading } = useUser();
 
   return (
-    <div className="fixed z-50 flex h-[calc(100vh_-_2rem)] w-[calc(100vw_-_2rem)] flex-col items-end">
+    <div className="fixed z-50 flex h-[90%] w-[calc(100vw_-_2rem)] flex-col items-end">
       {/* NOTE: ADD click outside func */}
       <aside className="mt-20 flex h-full w-60 min-w-max flex-col items-start gap-2 rounded-lg bg-primary-light/40 p-4 shadow-xl shadow-primary-dark backdrop-blur-lg backdrop-filter">
         {currentUser && <UserProfile />}
