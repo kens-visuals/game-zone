afa978934001fcaf36760d9f9ac1fa9a26cd8af8
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-02-09 14:34:06 -0800

    add mesage for empty collection

--- components/CollectionsDropdown.tsx
+++ components/CollectionsDropdown.tsx
@@ -30,60 +30,67 @@ export default function CollectionsDropdown({ game, isDropdownOpen }: Props) {
       }`}
     >
       <ul className="h-fit space-y-2 overflow-y-auto p-4 text-white">
-        {collections?.map((collection) => (
-          <li
-            key={collection.id}
-            className="flex items-center gap-2 rounded-lg bg-primary-light p-2 px-4 transition-all duration-300 hover:bg-primary-light/50"
-          >
-            <button
-              type="button"
-              onClick={() =>
-                manageCollection('add', collection.id!, {
-                  id,
-                  name,
-                  slug,
-                  background_image: backgroundImage,
-                  released,
-                  genres,
-                })
-              }
-              className="flex w-full items-center gap-2"
+        {collections?.length > 0 ? (
+          collections?.map((collection) => (
+            <li
+              key={collection.id}
+              className="flex items-center gap-2 rounded-lg bg-primary-light p-2 px-4 transition-all duration-300 hover:bg-primary-light/50"
             >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-4 w-4"
+              <button
+                type="button"
+                onClick={() =>
+                  manageCollection('add', collection.id!, {
+                    id,
+                    name,
+                    slug,
+                    background_image: backgroundImage,
+                    released,
+                    genres,
+                  })
+                }
+                className="flex w-full items-center gap-2"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
-                />
-              </svg>
-              {collection.name}
-
-              {!collection.isPublic && (
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
                   fill="none"
-                  className="ml-auto h-4 w-4 fill-primary-dark"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-4 w-4"
                 >
-                  <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
-                  <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
-                  <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 10.5v6m3-3H9m4.06-7.19l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z"
+                  />
                 </svg>
-              )}
-            </button>
+                {collection.name}
+
+                {!collection.isPublic && (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    className="ml-auto h-4 w-4 fill-primary-dark"
+                  >
+                    <path d="M3.53 2.47a.75.75 0 00-1.06 1.06l18 18a.75.75 0 101.06-1.06l-18-18zM22.676 12.553a11.249 11.249 0 01-2.631 4.31l-3.099-3.099a5.25 5.25 0 00-6.71-6.71L7.759 4.577a11.217 11.217 0 014.242-.827c4.97 0 9.185 3.223 10.675 7.69.12.362.12.752 0 1.113z" />
+                    <path d="M15.75 12c0 .18-.013.357-.037.53l-4.244-4.243A3.75 3.75 0 0115.75 12zM12.53 15.713l-4.243-4.244a3.75 3.75 0 004.243 4.243z" />
+                    <path d="M6.75 12c0-.619.107-1.213.304-1.764l-3.1-3.1a11.25 11.25 0 00-2.63 4.31c-.12.362-.12.752 0 1.114 1.489 4.467 5.704 7.69 10.675 7.69 1.5 0 2.933-.294 4.242-.827l-2.477-2.477A5.25 5.25 0 016.75 12z" />
+                  </svg>
+                )}
+              </button>
+            </li>
+          ))
+        ) : (
+          <li className="rounded-lg bg-primary-light/50 p-2 px-4 text-center text-body-2">
+            You should create some collections
           </li>
-        ))}
+        )}
       </ul>
+
       <Link
         href="/collections"
-        className="flex w-full items-center justify-center gap-2 rounded-b-md bg-black/20 p-2 text-body-2 backdrop-blur-lg backdrop-filter transition-all duration-300 hover:backdrop-blur-sm md:text-body-1"
+        className="flex w-full items-center justify-center gap-2 rounded-b-md bg-black/20 p-2 text-body-2 underline backdrop-blur-lg backdrop-filter transition-all duration-300 hover:backdrop-blur-sm md:text-body-1"
       >
         Create New Collection
         <svg
