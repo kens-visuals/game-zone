b7f4a2aef529bd4c5b042a6aa47068f4e8e033d2
Ken Nersisyan <karapet.nersisyan@yahoo.com>
2023-02-08 18:08:00 -0800

    add Divider animations

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,18 +1,28 @@
+import { motion } from 'framer-motion';
+
+// Animations
+import { dividerChildrenVariants, dividerVariants } from '../lib/animations';
+
 export default function Divider() {
   return (
-    <div className="my-4 grid w-full gap-4 lg:my-8">
-      <div className="relative">
+    <motion.div
+      initial="initial"
+      animate="animate"
+      variants={dividerVariants}
+      className="my-4 grid w-full gap-4"
+    >
+      <motion.div variants={dividerChildrenVariants} className="relative">
         <div className="absolute inset-0 h-1 w-full bg-secondary blur" />
         <div className="absolute inset-0 h-0.5 w-full bg-secondary" />
-      </div>
-      <div className="relative">
+      </motion.div>
+      <motion.div variants={dividerChildrenVariants} className="relative">
         <div className="absolute inset-0 h-1 w-full bg-secondary blur" />
         <div className="absolute inset-0 h-0.5 w-full bg-secondary" />
-      </div>
-      <div className="relative">
+      </motion.div>
+      <motion.div variants={dividerChildrenVariants} className="relative">
         <div className="absolute inset-0 h-1 w-full bg-secondary blur" />
         <div className="absolute inset-0 h-0.5 w-full bg-secondary" />
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 }
